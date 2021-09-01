b2ea81725d8b2bbf0ecf7cdb10895a779bbe8364
bariscodefxy <bariscodefx@gmail.com>
2021-09-01 17:45:23 +0300

    add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi this repository for learn React!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
